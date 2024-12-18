19bbcb631eb881df9385d993a8d5c4ddfff09690
Vladimir <sv_urich@mail.ru>
2024-12-18 10:54:42 +0400

    Add files via upload

--- module_16_1.py
+++ module_16_1.py
@@ -3,23 +3,26 @@ from pydantic import BaseModel
 
 app = FastAPI()
 
+@app.get("/")
+async def in_put()->str:
+    return "Главная страница"
+
 @app.get("/user/admin")
-async def get_admin(admin: str = 'Fedor'):
-    return {"message": f"Имя: {admin}, Вы вошли как администратор"}
+async def get_admin(admin: str = 'Fedor')->str:
+    return f"Имя: {admin}, Вы вошли как администратор"
 
 @app.get("/user/{user_id}")
-async def get_user(user_id: int):
-    return {"message": f"Вы вошли как пользователь № {user_id}"}
+async def get_user(user_id: int) ->str:
+    return f"Вы вошли как пользователь № {user_id}"
 
 @app.put("/user")
-async def put_user(username: str = 'Bob', age: int = 25) -> dict:
-    return {"message": f"Информация о пользователе. Имя: {username}, Возраст: {age}"}
+async def put_user(username: str = 'Bob', age: int = 25) ->str:
+    return f"Информация о пользователе. Имя: {username}, Возраст: {age}"
 
-@app.get("/")
-async def in_put():
-    return {"message": "Главная страница"}
 
-#  uvicorn main:app --reload
+
+#  uvicorn module_16_1:app --reload
 
 #  http://127.0.0.1:8000/docs
 #  http://127.0.0.1:8000/redoc
+
